17ecfece9413a55578638b8c2ce1a607fbe8f83c
teerkevin23 <kevinanthony.teer@gmail.com>
2023-05-26 18:38:11 -0700

    added boilerplate stub plus initial round of TDD setup testing for Rundoo takehome

---
+++ server/cmd/web/handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/internal/response"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	err := response.Page(w, http.StatusOK, data, "pages/home.tmpl")
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
---
+++ server/cmd/web/repository_test/productRepository.go
@@ -0,0 +1,33 @@
+package repository_test
+
+import (
+	"github.com/rundoo/server/cmd/web/domain"
+	"github.com/rundoo/server/cmd/web/repository"
+
+	"testing"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+const (
+	CollectionTest = "test"
+)
+
+func TestCreate(t *testing.T) {
+	mockProduct := &domain.Product{
+		ID: primitive.NewObjectID(),
+		Category:  CollectionTest,
+		Name: "TestProduct",
+		SKU: "123456",
+	}
+	mockEmptyProduct := &domain.Product{}
+	mockProductID := primitive.NewObjectID()
+
+	t.Run("success", func(t *testing.T) {
+		pr := repository.newProductRepository()
+		err := pr.Create(context.Background(), mockProduct)
+
+		assert.NoError(t, err)
+	})
+	t.Run("error", func(t *testing.T) {
+
+	})
+}
